52bc8d5ef88a36f9544961da15e3bed89873aff6
Aleksandre Gelashvili <>
2025-06-28 14:09:41 +0400

    Initial commit

---
+++ LumiformTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LumiformTask
+//
+//  Created by Aleksandre Gelashvili on 28.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LumiformTask/LumiformTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiformTaskApp.swift
+//  LumiformTask
+//
+//  Created by Aleksandre Gelashvili on 28.06.25.
+//
+
+import SwiftUI
+
+@main
+struct LumiformTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
